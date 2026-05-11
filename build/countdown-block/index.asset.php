4e2cbbbcc626fc9d7f58173abdd2d592ae34bfcd
Jamie <jamie7williams@gmail.com>
2026-05-11 19:19:48 +0100

    feat: add inspector controls for expiry message in edit.js

--- build/countdown-block/index.asset.php
+++ build/countdown-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '73cd1320bbfa949979de');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0024621648fabb05327b');
--- src/countdown-block/edit.js
+++ src/countdown-block/edit.js
@@ -1,14 +1,27 @@
 import { __ } from '@wordpress/i18n';
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import { TextControl, DateTime, PanelBody } from '@wordpress/components'
 import './editor.scss';
 
 // Represents what the editor will render
-export default function Edit() {
+export default function Edit( { attributes, setAttributes } ) {
 	return (
-		<p { ...useBlockProps() }>
-			<span>00 { __( 'Days', 'countdown-block') }</span>
-			<span>00 { __( 'Hours', 'countdown-block') }</span>
-			<span>00 { __( 'Minutes', 'countdown-block') }</span>
-		</p>
+		<>
+			<InspectorControls>
+				<PanelBody title="Countdown Settings">
+					<TextControl
+						label="Expiry message"
+						onChange={ ( expiryMessage ) => setAttributes( { expiryMessage: expiryMessage } ) }
+						value={ attributes.expiryMessage }
+					/>
+				</PanelBody>
+			</InspectorControls>
+
+			<p { ...useBlockProps() }>
+				<span>00 { __( 'Days', 'countdown-block') }</span>
+				<span>00 { __( 'Hours', 'countdown-block') }</span>
+				<span>00 { __( 'Minutes', 'countdown-block') }</span>
+			</p>
+		</>
 	);
 }
