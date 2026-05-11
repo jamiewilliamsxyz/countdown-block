afa5051f4a877b20f1664e6864416a0ccca5d9a4
Jamie <jamie7williams@gmail.com>
2026-05-11 18:44:09 +0100

    style: remove default styles and space out countdown span tags in style.scss

--- build/countdown-block/index.asset.php
+++ build/countdown-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '1c26bb2c8680899ee191');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'a41376e579bfb3cb6076');
